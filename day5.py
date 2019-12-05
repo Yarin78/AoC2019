6dd830a73edc01b4946cf39b38dbb844c07301ca
Jimmy Mårdell <jimmy.mardell@gmail.com>
2019-12-05 07:13:23 +0100

    day 5

---
+++ day5.py
@@ -0,0 +1,20 @@
+import sys
+from collections import defaultdict
+from lib import util
+from queue import Queue
+from lib.geo2d import *
+from lib.intcode import Program
+from aocd import data, submit
+
+# Make sure AOC_SESSION is updated! (Chrome inspector -> Application tab -> session)
+
+#lines = data.strip().split('\n')
+prog = Program(data, input=[5])
+
+# submit(?, part="a", day=5, year=2019)
+# submit(?, part="b", day=5, year=2019)
+#prog.log_info()
+#prog.show(0)
+
+prog.run()
+print(prog.output)
--- lib/intcode.py
+++ lib/intcode.py
@@ -8,18 +8,28 @@ UNSAFE_RW = True  # 4 times faster
 
 OPCODE_ADD = 1   # add (x), (y), (z)  =>  (z) = (x) + (y)
 OPCODE_MUL = 2   # mul (x), (y), (z)  =>  (z) = (x) * (y)
+OPCODE_IN = 3  # in (x)
+OPCODE_OUT = 4  # out (x)
+OPCODE_JUMP_TRUE = 5
+OPCODE_JUMP_FALSE = 6
+OPCODE_LESS_THAN = 7
+OPCODE_EQUALS = 8
 OPCODE_EXIT = 99
 
 class Program(object):
 
     opcodes = {}
 
-    def __init__(self, code):
+    def __init__(self, code, input=None):
         self.factory_settings = list(map(lambda x: int(x), code.strip().split(',')))
         self.reset()
         if UNSAFE_RW:
             self.read = self.unsafe_read
             self.write = self.unsafe_write
+
+        self.input = input
+        self.input_pos = 0
+        self.output = []
 
     def reset(self):
         self.mem = self.factory_settings[:]
@@ -68,25 +78,57 @@ class Program(object):
         if self.halted:
             raise MachineHaltedException()
         opcode = self.read(self.ip)
-        (instr, mnemonic, length) = self.opcodes[opcode]
+        param_mode = opcode // 100
+        opcode %= 100
+        (instr, mnemonic, length, write_par) = self.opcodes[opcode]
+        params = []
+        for i in range(1, length):
+            x = self.read(self.ip + i)
+            if i == write_par:
+                assert param_mode % 10 == 0
+            elif param_mode % 10 == 0:
+                x = self.read(x)
+            param_mode //= 10
+            params.append(x)
+
         if logger.isEnabledFor(logging.INFO):
-            logger.info('Executing %s' % mnemonic.format(*self.mem[self.ip+1:self.ip+length]))
+            logger.info('%5d: Executing %s' % (self.ip, self.decode(self.ip)))
         self.count += 1
         self.instr_count[self.ip] += 1
-        self.ip += 1
-        instr(self)
+        self.ip += length
+        instr(self, *params)
         return not self.halted
+
+    def decode(self, addr):
+        # Converts instruction to mnemonic
+        opcode = self.read(addr)
+        param_mode = opcode // 100
+        opcode %= 100
+        if opcode not in self.opcodes:
+            return 'DB %d' % opcode
+        (instr, mnemonic, length, write_par) = self.opcodes[opcode]
+        mnemonic += ' '
+        for i in range(1, length):
+            if i > 1:
+                mnemonic += ', '
+            x = self.read(addr + i)
+            if param_mode % 10 == 0:
+                mnemonic += '(%d)' % x
+            else:
+                mnemonic += '#%d' % x
+            param_mode //= 10
+        return mnemonic
 
     def show(self, addr):
         try:
             while addr < len(self.mem):
-                opcode = self.mem[addr]
+                opcode = self.mem[addr] % 100
                 if opcode in self.opcodes:
-                    (instr, mnemonic, length) = self.opcodes[opcode]
-                    line = '%5d %s' % (addr, mnemonic.format(*self.mem[addr+1:addr+length]))
+                    line = '%5d %s' % (addr, self.decode(addr))
                     if self.instr_count[addr]:
                         line = '%-30s [%d]' % (line, self.instr_count[addr])
                     print(line)
+                    (_, _, length, _) = self.opcodes[opcode]
                     addr += length
                 else:
                     print('%5d DB %d' % (addr, opcode))
@@ -100,27 +142,46 @@ class Program(object):
             if self.instr_count[i] > 0:
                 print('%5d %15d' % (i, self.instr_count[i]))
 
-    def opcode_add(self):
-        x = self.read(self.ip)
-        y = self.read(self.ip+1)
-        z = self.read(self.ip+2)
-        self.ip += 3
-        self.write(z, self.read(x)+self.read(y))
+    def opcode_add(self, x, y, z):
+        self.write(z, x + y)
 
-    def opcode_mul(self):
-        x = self.read(self.ip)
-        y = self.read(self.ip+1)
-        z = self.read(self.ip+2)
-        self.ip += 3
-        self.write(z, self.read(x)*self.read(y))
+    def opcode_mul(self, x, y, z):
+        self.write(z, x * y)
+
+    def opcode_in(self, x):
+        self.write(x, self.input[self.input_pos])
+        self.input_pos += 1
+
+    def opcode_out(self, x):
+        self.output.append(x)
+
+    def opcode_jump_true(self, x, y):
+        if x != 0:
+            self.ip = y
+
+    def opcode_jump_false(self, x, y):
+        if x == 0:
+            self.ip = y
+
+    def opcode_less_than(self, x, y, z):
+        self.write(z, 1 if x < y else 0)
+
+    def opcode_equals(self, x, y, z):
+        self.write(z, 1 if x == y else 0)
 
     def opcode_exit(self):
         self.halted = True
 
     opcodes = {
-        OPCODE_ADD: (opcode_add, 'ADD ({}), ({}), ({})', 4),
-        OPCODE_MUL: (opcode_mul, 'MUL ({}), ({}), ({})', 4),
-        OPCODE_EXIT: (opcode_exit, 'EXIT', 1),
+        OPCODE_ADD: (opcode_add, 'ADD', 4, 3),
+        OPCODE_MUL: (opcode_mul, 'MUL', 4, 3),
+        OPCODE_IN: (opcode_in, 'IN', 2, 1),
+        OPCODE_OUT: (opcode_out, 'OUT', 2, -1),
+        OPCODE_EXIT: (opcode_exit, 'EXIT', 1, -1),
+        OPCODE_JUMP_TRUE: (opcode_jump_true, 'JMPT', 3, -1),
+        OPCODE_JUMP_FALSE: (opcode_jump_false, 'JMPF', 3, -1),
+        OPCODE_LESS_THAN: (opcode_less_than, 'LT', 4, 3),
+        OPCODE_EQUALS: (opcode_equals, 'EQ', 4, 3)
     }
 
 
