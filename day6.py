8c3e6c83e7d8b6933e531564f73787b40af9a872
Jimmy Mårdell <jimmy.mardell@gmail.com>
2019-12-06 07:12:11 +0100

    Cleaning up day6

--- day6.py
+++ day6.py
@@ -4,60 +4,18 @@ from lib import util
 from queue import Queue
 from lib.geo2d import *
 from lib.intcode import *
+from lib.tree import Tree
 from aocd import data, submit
 
-# Make sure AOC_SESSION is updated! (Chrome inspector -> Application tab -> session)
-
+# Cleaned up version; see day6.org.py for hacky submitted version
 
 lines = data.strip().split('\n')
 
-parent = {}
-children = {}
-for line in lines:
-    [a,b] = line.split(')')
-    parent[b] = a
-    if a not in children:
-        children[a] = []
-    children[a].append(b)
+tree = Tree([line.split(')') for line in lines])
 
-tot_count = 0
+print(sum(tree.depth[c] for c in tree.children.keys()))
 
-def traverse(cur, depth):
-    global tot_count
-    tot_count += depth
-    if cur in children:
-        for c in children[cur]:
-            traverse(c, depth+1)
+a1 = tree.all_ancestors('YOU')
+a2 = tree.all_ancestors('SAN')
 
-traverse('COM', 0)
-
-
-#prog = intcode.Program(data)
-print(tot_count)
-# submit(?, part="a", day=6, year=2019)
-# submit(?, part="b", day=6, year=2019)
-
-def chain(cur, l):
-    l.append(cur)
-    if cur in parent:
-        chain(parent[cur], l)
-
-yc=[]
-sc=[]
-chain('YOU', yc)
-chain('SAN', sc)
-
-dist={}
-c=0
-for y in yc:
-    dist[y] = c
-    c += 1
-
-c = 0
-for s in sc:
-    if s in dist:
-        print(dist[s] + c - 2)
-        exit()
-    c += 1
-
-
+print(min([a1.index(c) + a2.index(c) for c in a1 if c in a2]))
