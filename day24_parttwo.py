e2417e66d34e1f30ddac662d5afaa43526672c5c
Jimmy Mårdell <jimmy.mardell@gmail.com>
2019-12-24 10:29:45 +0100

    clean up day 24

--- day24_parttwo.py
+++ day24_parttwo.py
@@ -31,39 +31,26 @@ for minutes in range(200):
                 for d in DIRECTIONS:
                     np = p+d
                     if np.x < 0:
-                        if levels[lvl-1][2][1] == '#':
-                            cnt += 1
+                        cnt += levels[lvl-1][2][1] == '#'
                     elif np.x >= 5:
-                        if levels[lvl-1][2][3] == '#':
-                            cnt += 1
+                        cnt += levels[lvl-1][2][3] == '#'
                     elif np.y < 0:
-                        if levels[lvl-1][1][2] == '#':
-                            cnt += 1
+                        cnt += levels[lvl-1][1][2] == '#'
                     elif np.y >= 5:
-                        if levels[lvl-1][3][2] == '#':
-                            cnt += 1
+                        cnt += levels[lvl-1][3][2] == '#'
                     elif np.y == 2 and np.x == 2:
                         if x == 1:
-                            for ny in range(5):
-                                if levels[lvl+1][ny][0] == '#':
-                                    cnt += 1
+                            cnt += sum(levels[lvl+1][ny][0] == '#' for ny in range(5))
                         elif x == 3:
-                            for ny in range(5):
-                                if levels[lvl+1][ny][4] == '#':
-                                    cnt += 1
+                            cnt += sum(levels[lvl+1][ny][4] == '#' for ny in range(5))
                         elif y == 1:
-                            for nx in range(5):
-                                if levels[lvl+1][0][nx] == '#':
-                                    cnt += 1
+                            cnt += levels[lvl+1][0].count('#')
                         elif y == 3:
-                            for nx in range(5):
-                                if levels[lvl+1][4][nx] == '#':
-                                    cnt += 1
+                            cnt += levels[lvl+1][4].count('#')
                         else:
                             assert False
                     else:
-                        if levels[lvl][np.y][np.x] == '#':
-                            cnt += 1
+                        cnt += levels[lvl][np.y][np.x] == '#'
 
                 if levels[lvl][p.y][p.x] == '#':
                     if cnt == 1:
